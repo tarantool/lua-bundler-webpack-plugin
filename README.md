79c461ef35e3d7b4bac5193372e614ac38d92360
Nikolay Mendyaev <n.mendyaev@corp.mail.ru>
2019-08-14 17:43:08 +0300

    try to builtin

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
-#lua-bundler
+# Lua Bundler Webpack Plugin
 
-##Custom options
-bundleName - name of bundle<br/>
-namespace - ...<br/>
-entryRegExp - regExp for file names<br/>
-<br/><br/>
-Example: 
+## Install
+```
+npm i -s @tarantool-front/lua-bundler-webpack-plugin
+```
+
+## Custom options
+
+`bundleName` - output bundle path in your build directory. By default: `./bundle.lua`
+
+`namespace` - namespace of your module. You should place your files under namespace in your build for exclude clash of namespacing with other modules.
+
+`entryRegExp` - regExp for using some js files as entry. `/main.+js/` is using by default. It's used by `@tarantool-front/core` rocks as entry files.
+
+`lua` - path to tarantool. By default we assume that you need install [tarantool](https://www.tarantool.io/ru/). We use some built-in modules in our packer.
+
+
+Example:
 ```
 plugins: [
 ....
@@ -13,13 +24,13 @@ new LuaBundlePlugin({bundleName: 'customBundleName.lua', namespace: 'cluster', e
 ]
 ```
 
-##Defaults
+## Defaults
 
 bundleName = 'bundle.lua'<br/>
 namespace = ''<br/>
 entryRegExp: /main.+js$/<br/>
 
-##Debug
+## Debug
 
 ```
 DEBUG='lua-bundler' npm run build
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mainFs = require('fs');
 const cp = require('child_process');
 const debug = require('debug')('lua-bundler');
+const dir = __dirname;
 
 const walkSync = function(dir, filelist) {
   const files = mainFs.readdirSync(dir);
@@ -20,31 +21,38 @@ const walkSync = function(dir, filelist) {
 
 class LuaBundlePlugin {
   constructor(options) {
-    this.options = options || {}
+    this.options = Object.assign(
+      {
+        bundleName: 'bundle.lua',
+        namespace: '',
+        entryRegExp: /main.+js$/,
+        lua: 'tarantool',
+      },
+      options
+    )
 
   }
   apply(compiler) {
     compiler.hooks.afterEmit.tap('LuaBundlePlugin', compilation => {
       const outputPath = compiler.options.output.path;
+      const { bundleName, namespace, entryRegExp, lua } = this.options
       const buildFoler = path.relative(process.cwd(), outputPath);
-      const bundleName = this.options.bundleName|| 'bundle.lua';
-      const namespace = this.options.namespace || '';
       const namespaceFolder = buildFoler + '/static/' + (namespace ? namespace + '/' : '');
       const files = walkSync(namespaceFolder);
       const filemap = {};
-      const isEntry = this.options.entryRegExp || /main.+js$/;
       for (const file of files){
         const fileName = file.slice(namespaceFolder.length);
         const fileBody = mainFs.readFileSync(file, { encoding: 'utf8' });
         filemap[fileName] = {
-          is_entry: isEntry.test(fileName),
+          is_entry: entryRegExp.test(fileName),
           body: fileBody,
           mime: mime.lookup(fileName)
         }
       }
+      const pathToPacker = path.join(dir, 'pack-front.lua')
       mainFs.writeFileSync(buildFoler + '/bundle.json', JSON.stringify(filemap), { encoding: 'utf8' })
       debug('compile bundle.json')
-      cp.execSync('tarantool -l pack-front - build/bundle.json build/' + bundleName)
+      cp.execSync(lua + ' -l ' + pathToPacker+ ' - build/bundle.json build/' + bundleName)
       debug('build ' + bundleName)
       mainFs.unlinkSync(buildFoler + '/bundle.json')
       debug('delete bundle.json')
